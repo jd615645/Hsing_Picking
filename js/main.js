13ec3832257115443074c394a8ad830e42b44741
jd615645 <jd615645@gmail.com>
2016-01-31 01:22:01 +0800

    :cat2:搜尋課程欄位選單完成

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 jQuery(document).ready(function($) {
-
+	window.dropdown = {};
+	window.search_item = [];
+	window.search_detail = [];
+	window.select_department = [];
+	$.getJSON("json/select.json",function(data){
+		// console.log(data);
+		for(var i=0 ; i < data.length ; i++)
+		{
+			if(typeof(window.dropdown[data[i].searchItem]) == 'undefined')
+				window.dropdown[data[i].searchItem] = [];
+			
+			window.dropdown[data[i].searchItem].push(data[i].searchDetail);
+		}
+		$('.dropdown').dropdown();
+	})
+	
+	function select_item(item) {
+		$('#search-detail').empty();
+		$('#search-detail').dropdown('clear');
+		$('.search.selection .default.text').text('選取分類');
+		$.each(window.dropdown[item][0], function(key, val) {
+			var html = $.parseHTML("<option value=\"" + val.value +"\">" + val.name + "<\/option>");
+			$('#search-detail').append(html);
+		});
+	}
+	$('#search-item').change(function() {
+		select_item($(this).dropdown('get value'));
+	});
 });
--- js/tools.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function($) {
-	$('#serch_type').dropdown();
-	$('#select_degree').dropdown();
-});
