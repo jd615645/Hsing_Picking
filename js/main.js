63000e195bcf615edea4b4e21563ba17e7a6f85c
jd615645 <jd615645@gmail.com>
2016-02-01 01:03:03 +0800

    :cat2:新增前端介面樣式

--- js/main.js
+++ js/main.js
@@ -14,17 +14,50 @@ jQuery(document).ready(function($) {
 		}
 		$('.dropdown').dropdown();
 	})
-	
-	function select_item(item) {
+
+	// 根據選取項目改變其他選單內容
+	function select_item(value) {
 		$('#search-detail').empty();
 		$('#search-detail').dropdown('clear');
 		$('.search.selection .default.text').text('選取分類');
-		$.each(window.dropdown[item][0], function(key, val) {
+		$.each(window.dropdown[value][0], function(key, val) {
 			var html = $.parseHTML("<option value=\"" + val.value +"\">" + val.name + "<\/option>");
 			$('#search-detail').append(html);
 		});
 	}
+	function degree_change(value) {
+		var level = ['一年級', '二年級', '三年級', '四年級', '五年級',]
+		var department = 2, html;
+		$('#select-department .menu').empty();
+		$('#select-department .text').text('選取科系');
+		$('#select-level .menu').empty();
+		$('#select-level .text').text('選取年級');
+		// $('#select-level .menu').empty();
+
+		if(value == '學士班' || value == '進修學士班')
+			department = 5;
+
+		for (var i = 0; i < department; i++) {
+			html = $.parseHTML("<div class=\"item\" value=\"" + i +"\">" + level[i] + "<\/div>");
+			$('#select-level .menu').append(html);
+		};
+
+		$.each(window.dropdown[value][0], function(key, val) {
+			html = $.parseHTML("<div class=\"item\" value=\"" + val.value +"\">" + val.name + "<\/div>");
+			$('#select-department .menu').append(html);
+		});
+
+
+	}
+
+	// 搜尋選單監聽
 	$('#search-item').change(function() {
 		select_item($(this).dropdown('get value'));
 	});
+	// 系級選單監聽
+	$('#select-degree').change(function() {
+		degree_change($(this).dropdown('get value'));
+	});
+	$('#tools-btn .ui.button').popup();
+	$('#serch-sol .ui.button').popup({position : 'bottom right'});
 });
