a30cd01b0ab9827d26e79ef08d35b7a48e1365e6
jd615645 <jd615645@gmail.com>
2017-01-17 02:38:12 +0800

    :cat2:

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var lessMiddleware = require('less-middleware');
 
-var routes = require('./routes/index');
+var index = require('./routes/index');
 
 var app = express();
 
@@ -19,9 +20,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use(lessMiddleware({
+//   dest: __dirname + '\\public\\styles',
+//   src: __dirname + '\\public\\styles',
+//   compress: true
+// }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
 
-app.use('/', routes);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,29 +37,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- public/js/new.js
+++ public/js/new.js
@@ -20,15 +20,32 @@ $(document).ready(function() {
   // time view switch
   $('#timeSwitch').click(function() {
     var viewType = $(this).find('a').attr('view');
-    $('#timeSwitch .active').removeClass('active');
+    $('#timeSwitch').find('.active').removeClass('active');
     if (viewType == 'day') {
       $(this).find('a').attr('view', 'night');
+      viewType = 'night';
     }
     else {
       $(this).find('a').attr('view', 'day');
+      viewType = 'day';
     }
     $('#timeSwitch i[type="' + viewType + '"]').addClass('active');
+  })
 
+  // schedule view type
+  $('#viewSwitch').click(function() {
+    var viewType = parseInt($(this).find('a').attr('view'));
+    $('#viewSwitch').find('.active').removeClass('active');
+
+    if (viewType < 4) {
+      viewType += 1;
+    }
+    else {
+      viewType = 1;
+    }
+
+    $(this).find('a').attr('view', viewType);
+    $('#viewSwitch i[value="' + viewType + '"]').addClass('active');
   })
 
   $('#courseTab .tab-nav li[value="1"]').click();
