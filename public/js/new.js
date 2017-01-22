3280701e1ee614d1e35e99750335604c182f71b5
jd615645 <jd615645@gmail.com>
2017-01-22 16:37:09 +0800

    :cat2:update

--- public/js/new.js
+++ public/js/new.js
@@ -1,52 +1,31 @@
 $(document).ready(function() {
-  var menuTab = 1;
+  window.departmentData = {};
 
-  $('#courseTab .tab-nav li').click(function() {
-    var tabVal = parseInt($(this).attr('value')),
-        nowTab = parseInt($('#courseTab li.active').attr('value'));
+  // var selectedDept;
+  window.selectedDept;
 
-    if (tabVal != nowTab) {
-      $('#courseTab li.active').removeClass('active');
-      $('#courseTab li[value="' + tabVal + '"]').addClass('active');
-
-      $('#title h4.active').removeClass('active');
-      $('#title h4[value="' + tabVal + '"]').addClass('active');
-
-      $('#toolTab .active').removeClass('active');
-      $('#toolTab [value="' + tabVal + '"]').addClass('active');
-    }
+  // 引入選單資料
+  var getSelect = $.getJSON('./json/select.json',function(data) {
+    $.each(data, function(key, val) {
+      var item = val['searchItem'],
+      detail = val['searchDetail'];
+      departmentData[key] = detail;
+    });
+    selectedDept = departmentData[0];
   });
 
-  // time view switch
-  $('#timeSwitch').click(function() {
-    var viewType = $(this).find('a').attr('view');
-    $('#timeSwitch').find('.active').removeClass('active');
-    if (viewType == 'day') {
-      $(this).find('a').attr('view', 'night');
-      viewType = 'night';
-    }
-    else {
-      $(this).find('a').attr('view', 'day');
-      viewType = 'day';
-    }
-    $('#timeSwitch i[type="' + viewType + '"]').addClass('active');
+  getSelect.then(function(){
+    var vm = new Vue({
+      el: '#app',
+      data() {
+        return {
+          notifyKeep: 0,
+          notifyCourse: 0,
+          credits: 0,
+          selectedDept: selectedDept,
+          tabView: 0
+        }
+      }
+    });
   })
-
-  // schedule view type
-  $('#viewSwitch').click(function() {
-    var viewType = parseInt($(this).find('a').attr('view'));
-    $('#viewSwitch').find('.active').removeClass('active');
-
-    if (viewType < 4) {
-      viewType += 1;
-    }
-    else {
-      viewType = 1;
-    }
-
-    $(this).find('a').attr('view', viewType);
-    $('#viewSwitch i[value="' + viewType + '"]').addClass('active');
-  })
-
-  $('#courseTab .tab-nav li[value="1"]').click();
 });
