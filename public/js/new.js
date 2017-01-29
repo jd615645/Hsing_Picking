782cc5b2410bcf57300fc99ca7253b2a0d2c5d1c
jd615645 <jd615645@gmail.com>
2017-01-30 01:04:12 +0800

    :cat2: search & ui change add keyword search title menu ui change

--- public/js/new.js
+++ public/js/new.js
@@ -12,12 +12,14 @@ var vm = new Vue({
   el: '#app',
   data() {
     return {
-      // tab-nav
-      selectItem: '-1',
-      selectDetail: '-1',
+      // search
+      searchKeyword: '',
+      searchItem: '-1',
+      searchDetail: '-1',
       detailData: [''],
-      selectTime: '-1',
-      // 顯示堂數
+      searchTime: '-1',
+      searchCourse: [],
+      // 顯示節數
       tableView: 8,
       viewSwitch: 0,
       // keepTab
@@ -108,6 +110,7 @@ var vm = new Vue({
           level = this.selectLevel;
 
       this.clearSelected();
+      // 區分AB班
       if (dept.slice(-1) == 'A' || dept.slice(-1) == 'B') {
         level = level + dept.slice(-1);
         dept = dept.replace(/ A| B/g,'');
@@ -116,17 +119,26 @@ var vm = new Vue({
       this.pickingCourse = [];
       this.credits = 0;
       $.each(departmentData[year][dept][level], (key, code) => {
+        //  確認是否必修
         if (courseCode[year][code]['obligatory_tf']) {
-          this.addSelected(code);
+          if (courseCode[year][code]['title'].match('專題') == null) {
+            this.addCourse(code);
+          }
+          else {
+            this.addKeep(code);
+          }
         }
         else {
           this.addKeep(code);
         }
       });
     },
+    // tool menu change selected
     changeItem(item) {
       console.log(item);
       this.detailData = departmentData[item];
+      this.searchDetail = -1;
+      this.searchTime = -1;
     },
     changeDetail(detail) {
       console.log(detail);
@@ -134,23 +146,42 @@ var vm = new Vue({
     changeTime(time) {
       console.log(time);
     },
-    addSelected(code) {
-      var year = this.selectYear;
+    addCourse(code) {
+      if(this.isFree(code)) {
+        var year = this.selectYear;
+        var removeSpace = _.findIndex(this.keepCourse, {code: code});
 
-      this.credits += courseCode[year][code]['credits_parsed'];
-      this.pickingCourse.push(courseCode[year][code]);
-      $.each(courseCode[year][code]['time_parsed'], (ik, iv) => {
-        $.each(iv.time, (jk, jv) => {
-          var day = iv.day,
-              time = jv;
-          // console.log('day:' + day);
-          // console.log(jv);
-          this.schedule[time-1][day-1] = courseCode[year][code];
+        // add credits
+        this.credits += courseCode[year][code]['credits_parsed'];
+
+        // if sourse is keep remove item
+        if(removeSpace != -1) {
+          this.keepCourse.splice(removeSpace, 1);
+        }
+
+        // add course to picking list
+        this.pickingCourse.push(courseCode[year][code]);
+        $.each(courseCode[year][code]['time_parsed'], (ik, iv) => {
+          $.each(iv.time, (jk, jv) => {
+            var day = iv.day,
+            time = jv;
+            this.schedule[time-1][day-1] = courseCode[year][code];
+          });
         });
-      });
+      }
+      else {
+        alert('衝堂');
+      }
     },
     addKeep(code) {
       var year = this.selectYear;
+      var removeSpace = _.findIndex(this.searchCourse, {code: code});
+
+      // if sourse is keep remove item
+      if(removeSpace != -1) {
+        this.searchCourse.splice(removeSpace, 1);
+      }
+
       this.keepCourse.push(courseCode[year][code]);
     },
     clearSelected() {
@@ -163,8 +194,93 @@ var vm = new Vue({
         });
       });
     },
-    clrBtn() {
+    removeCourse(code, type) {
+      var year = this.selectYear;
 
+      if (type == 'search') {
+        // remove list course
+        var removeSpace = _.findIndex(this.searchCourse, {code: code});
+
+        this.searchCourse.splice(removeSpace, 1);
+      }
+      else if (type == 'keep') {
+        var removeSpace = _.findIndex(this.keepCourse, {code: code});
+
+        this.keepCourse.splice(removeSpace, 1);
+      }
+      else if (type == 'now') {
+        var removeSpace = _.findIndex(this.pickingCourse, {code: code});
+
+        this.pickingCourse.splice(removeSpace, 1);
+        // remove table course
+        $.each(courseCode[year][code]['time_parsed'], (ik, iv) => {
+          $.each(iv.time, (jk, jv) => {
+            var day = iv.day,
+            time = jv;
+            this.schedule[time-1][day-1] = [];
+          });
+        });
+
+        // less credits
+        this.credits -= courseCode[year][code]['credits_parsed'];
+      }
+    },
+    // 課程搜尋
+    searchData() {
+      var year = this.selectYear;
+          keyword = this.searchKeyword,
+          item = this.searchItem,
+          detail = this.searchDetail,
+          time = this.searchTime;
+      var filteredCourse = [];
+      if (keyword != '') {
+        if (keyword.length >1) {
+          filteredCourse = _.filter(courseCode[year], (course) => {
+            if (!(_.isUndefined(course))) {
+              return course['code'] == keyword ||
+                     course['professor'].indexOf(keyword) > -1 ||
+                     course['title_parsed']['zh_TW'].indexOf(keyword) > -1;
+            }
+          });
+        }
+        else {
+          alert('關鍵字請大於2個字');
+        }
+        console.log(filteredCourse);
+      }
+      this.searchCourse = filteredCourse;
+      // console.log('year: ' + year);
+      // console.log('searchKeyword: ' + searchKeyword);
+      // console.log('searchItem: ' + searchItem);
+      // console.log('searchDetail: ' + searchDetail);
+      // console.log('searchTime: ' + searchTime);
+    },
+    // 判斷是否衝堂
+    isFree(code) {
+      var year = this.selectYear;
+      var free = true;
+      $.each(courseCode[year][code]['time_parsed'], (ik, iv) => {
+        $.each(iv.time, (jk, jv) => {
+          var day = iv.day,
+              time = jv;
+          if(!(this.schedule[time-1][day-1])) {
+            free = false;
+          }
+        });
+      });
+      return free;
+    },
+    clearAll() {
+
+    },
+    clearSearch() {
+      this.searchCourse = [];
+    },
+    clearKeep() {
+      this.keepCourse = [];
+    },
+    clearCourse() {
+      this.clearSelected();
     },
     getOnepiceUrl(num) {
       return 'https://onepiece.nchu.edu.tw/cofsys/plsql/Syllabus_main_q?v_strm=' + this.selectYear + '&v_class_nbr=' + num;
